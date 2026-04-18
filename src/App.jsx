67229c6446695b68af6ddaa5d32627ab10779779
yurikas-dev <156330912+yurikas-dev@users.noreply.github.com>
2026-04-17 22:32:21 -0400

    Add React Router routing and Vercel SPA config

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Projects from './components/Projects'
-import AboutSkills from './components/AboutSkills'
-// import About from './components/About'
-// import Skills from './components/Skills'
-import Experience from './components/Experience'
-import Contact from './components/Contact'
-import ProjectsPage from './pages/ProjectsPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
+import AboutSkills from './components/AboutSkills';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
+import ProjectsPage from './pages/ProjectsPage';
 
 function HomePage() {
   return (
@@ -18,22 +16,20 @@ function HomePage() {
         <Hero />
         <Projects />
         <AboutSkills />
-        {/* <About /> */}
-        {/* <Skills /> */}
         <Experience />
         <Contact />
       </main>
     </>
-  )
+  );
 }
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/projects' element={<ProjectsPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
